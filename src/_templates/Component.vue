242853f370595103c84a389cbdc9ae1eb3136cd0
Patrick Weissenberger <patrick@patrick.local>
2024-03-07 16:23:02 +0100

    created _templates for .vue files

---
+++ src/_templates/Component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+
+
+<script>
+
+</script>
---
+++ src/_templates/View.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1></h1>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+
+
+<script>
+
+</script>
